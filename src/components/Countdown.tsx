a24679688544ab91e871bd4059f4d1026b30b85b
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-02-23 13:40:31 -0300

    countdown component complete

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,17 +2,24 @@ import style from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
     return (
-        <div className={style.countdownContainer}>
+        <div>
 
-            <div>
-                <span>2</span>
-                <span>5</span>
+            <div className={style.countdownContainer}>
+
+                <div>
+                    <span>2</span>
+                    <span>5</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
+
+            <button type='button' className={style.CountDownButton}>
+                Iniciar um ciclo
+            </button>
         </div>
     );
 }
