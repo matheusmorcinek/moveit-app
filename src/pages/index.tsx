1a795fe5781ae2bd52c749a7c0374eecf6a270be
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-02-23 11:00:40 -0300

    Complete, base project React using Typescript and the Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Head from 'next/head';
+// import Head from 'next/head';
 import { ExperienceBar } from "../components/ExperienceBar";
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
+      {/* <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
           rel="stylesheet" />
-      </Head>
+      </Head> */}
       <ExperienceBar></ExperienceBar>
     </div>
   )
