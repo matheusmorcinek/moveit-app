d322c2ae7d5219e5edd3555093316f32417738f3
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-02-23 12:04:42 -0300

    created completedChallanges component

---
+++ src/components/CompletedChallanges.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/components/CompletedChallanges.module.css';
+
+export function CompletedChallenges() {
+
+    return (
+        <div className={styles.completedChallangesContainer}>
+            <span>Desafios completos</span>
+            <span>5</span>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CompletedChallenges } from "../components/CompletedChallanges";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
@@ -6,12 +7,13 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-     
+
       <ExperienceBar></ExperienceBar>
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
         <div></div>
       </section>
